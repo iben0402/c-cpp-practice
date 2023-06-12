66ad36a4a7155edb06640314e728b482b152b2dd
iben0402 <81482747+iben0402@users.noreply.github.com>
2023-06-12 19:22:18 +0200

    Valid anagram problem solved

---
+++ anagram/anagram.cpp
@@ -0,0 +1,37 @@
+#include "iostream"
+#include "vector"
+
+using namespace std;
+
+bool isAnagram(string s, string t) {
+    if(s==t) return true;
+    if(s.length()!=t.length) return false;
+
+    vector<int> amounts(26, 0);
+
+    for(int i=0; i<s.length(); i++) {
+        amounts[s[i]-'a']++;
+        amounts[t[i]-'a']--;
+    } 
+
+    for(int i=0; i<amounts.size(); i++) {
+        if(amounts[i]!=0) return false;
+    }
+
+    return true;
+
+}
+
+int main() {
+
+    string s="anagram", t="nagaram";
+
+    cout << s << " " << t << ": " << isAnagram(s, t);
+
+    s = "rat";
+    t = "car";
+
+    cout << s << " " << t << ": " << isAnagram(s, t);
+
+
+}
